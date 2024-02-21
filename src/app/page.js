ffae17102a5f1c97335b0274adbf77e8d2a7f8c3
Hammad Javed <hammadjaved265@gmail.com>
2024-02-21 17:27:30 +0500

    new

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main>
-
+      <button className="bg-[#f33b3b] text-white p-2 rounded-lg float-end mr-4">+ Create Dashboard</button>
     </main>
   );
 }
